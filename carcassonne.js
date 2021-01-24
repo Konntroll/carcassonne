18ffacff0e6bca8386cb8ddac0a568848f2bffa0
konntroll <grigory.koltashev@gmail.com>
2021-01-24 20:38:32 +0000

    Started implementing rooms, finalized claims, basic UI

--- carcassonne.js
+++ carcassonne.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const carcass = express();
-const http = require('http').Server(carcass);
+const carcassonne = express();
+const http = require('http').Server(carcassonne);
 const io = require('socket.io')(http);
 const path = require('path');
 const fs = require('fs')
@@ -12,40 +12,96 @@ for (const [tile, sides] of Object.entries(JSON.parse(data))) {
     tiles.set(parseInt(tile), sides);
 }
 
-let keys = mix();
-
-function mix() {
+function fill() {
   let bag = [];
   for (let i = 0; i < 71; i++) {
     bag.push(i);
   }
-  for (let i = bag.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [bag[i], bag[j]] = [bag[j], bag[i]];
-  }
   return bag;
 }
+function mix(set) {
+  for (let i = set.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [set[i], set[j]] = [set[j], set[i]];
+  }
+  return set;
+}
 
-carcass.get('/', function(req, res) {
+carcassonne.get('/', function(req, res) {
    res.sendFile('carcassonne.html', {root: __dirname});
 }).use(express.static(path.join(__dirname, '/public')));
 
+let games = new Map();
+
 io.on('connection', function(socket) {
-  let key = keys.shift();
-  socket.emit('tile', {sides: tiles.get(key),
-                       key: key,
-                       rot: 0,
-                       claim: {by: null}
-                      });
-  socket.on('placed', function() {
-    let key = keys.shift();
-    socket.emit('tile', {sides: tiles.get(key),
-                         key: key,
+  socket.emit('welcome', listGames());
+
+  socket.on('create', function(game) {
+    games.set(game, {
+      name: game,
+      colors: mix(['white', 'green', 'blue', 'red', 'yellow', 'orange']),
+      bag: mix(fill()),
+      players: [],
+      started: false
+    });
+    socket.game = games.get(game);
+    socket.game.players.push(socket);
+    socket.color = socket.game.colors.shift();
+    socket.join(game);
+    socket.emit('newPlayer', socket.color);
+    io.emit('updateGamesAvailable', game);
+  });
+
+  socket.on('join', function(game) {
+    socket.game = games.get(game);
+    socket.game.players.push(socket);
+    socket.color = socket.game.colors.shift();
+    socket.join(game);
+    socket.emit('newPlayer', socket.color);
+  });
+
+  socket.on('leave', function() {
+    socket.leave(socket.game.name);
+    let dropout = socket.game.players.indexOf(socket.id);
+    socket.game.players.splice(dropout, 1);
+    socket.color = '';
+    socket.emit('left');
+    if (socket.game.players.length <= 1) {
+      closeGame();
+    }
+    socket.game = null;
+    console.log(socket);
+  });
+
+  socket.on('ready', function(board) {
+    socket.game.started = true;
+    io.emit('updateGamesAvailable', socket.game.name);
+    let tile = socket.game.bag.shift();
+    io.in(socket.game.name).emit('update', board);
+    io.to(socket.game.players[0].id).emit('tile',
+                        {sides: tiles.get(tile),
+                         key: tile,
                          rot: 0,
                          claim: {by: null}
-                        });
+                        }
+    );
+    socket.game.players.push(socket.game.players.shift());
   });
 });
+
+function listGames() {
+  let gamesAvailable = []
+  for (let [key, game] of games.entries()) {
+    if (game.colors.length != 0 &&
+        !game.started) {
+      gamesAvailable.push(key);
+    }
+  }
+  return gamesAvailable;
+};
+function closeGame() {
+  return;
+};
 
 http.listen(port, function() {
    console.log('Listening on port ' + port);
